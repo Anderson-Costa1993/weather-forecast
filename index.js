dcaa51a6828f783090075c792deb4848dd136c93
Anderson-Costa1993 <111035530+Anderson-Costa1993@users.noreply.github.com>
2023-07-24 17:04:34 -0300

    projeto previsão do tempo

---
+++ index.js
@@ -0,0 +1,24 @@
+const key = "d6cf77531fd16000c016c56ec9f3df72"
+
+function renderizaDados (dados) {
+  document.querySelector(".cidade").innerHTML = "Tempo em: " + dados.name;
+  document.querySelector(".temperatura").innerHTML =   Math.floor(dados.main.temp) + " C°";
+  document.querySelector(".temperatura-min").innerHTML = "Min " + Math.floor(dados.main.temp_min) + "°"
+  document.querySelector(".temperatura-max").innerHTML = "Max " + Math.floor(dados.main.temp_max) + "°"
+  document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description;
+  document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
+  document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+  console.log(dados)
+}
+
+async function buscaCidade(city) {
+const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+renderizaDados(dados)
+}
+
+function cliqueiNoBotao () {
+  const city = document.querySelector(".input-cidade").value
+  buscaCidade(city)
+}
+
+cliqueiNoBotao ()
