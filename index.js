0b0714cb3ba92344240d6135a95244ad11e3c80d
Anderson-Costa1993 <111035530+Anderson-Costa1993@users.noreply.github.com>
2023-07-25 11:36:15 -0300

    Alterações de validação

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const key = "d6cf77531fd16000c016c56ec9f3df72"
 
 function renderizaDados (dados) {
-  document.querySelector(".cidade").innerHTML = "Tempo em: " + dados.name;
+  document.querySelector(".cidade").innerHTML =  dados.name;
   document.querySelector(".temperatura").innerHTML =   Math.floor(dados.main.temp) + " C°";
   document.querySelector(".temperatura-min").innerHTML = "Min " + Math.floor(dados.main.temp_min) + "°"
   document.querySelector(".temperatura-max").innerHTML = "Max " + Math.floor(dados.main.temp_max) + "°"
@@ -13,12 +13,19 @@ function renderizaDados (dados) {
 
 async function buscaCidade(city) {
 const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
-renderizaDados(dados)
+
+if (city == dados.name) {
+  renderizaDados(dados)
+} else {
+  document.querySelector(".input-cidade").value = "São Paulo"
+  return alert ('Cidade não encontrada! Respeite letras maiusculas e minusculas' )
+}
 }
 
 function cliqueiNoBotao () {
   const city = document.querySelector(".input-cidade").value
   buscaCidade(city)
 }
+
 
 cliqueiNoBotao ()
